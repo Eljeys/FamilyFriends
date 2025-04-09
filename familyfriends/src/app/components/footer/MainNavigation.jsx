88fc78fe434f5123f7f2a17bf325205d88ccc2b0
eljeys <eljeys1@gmail.com>
2025-04-09 14:27:06 +0200

    MainNavigation done

--- familyfriends/src/app/components/footer/MainNavigation.jsx
+++ familyfriends/src/app/components/footer/MainNavigation.jsx
@@ -1,17 +1,39 @@
 "use client";
+import { useState } from "react";
 import { GoHome } from "react-icons/go";
 import { GoStar } from "react-icons/go";
 import { HiChatBubbleOvalLeft } from "react-icons/hi2";
 import { LuUser } from "react-icons/lu";
 
 const MainNavigation = () => {
+  const [activeItem, setActiveItem] = useState(null);
+
+  const menuItems = [
+    { id: "home", icon: <GoHome size={24} /> },
+    { id: "favorite", icon: <GoStar size={24} /> },
+    { id: "message", icon: <HiChatBubbleOvalLeft size={24} /> },
+    { id: "profile", icon: <LuUser size={24} /> },
+  ];
+
+  const handleItemClick = (id) => {
+    setActiveItem(id);
+  };
   return (
-    <div className="flex gap-8 fixed">
-      <GoHome size={24} />
-      <GoStar size={24} />
-      <HiChatBubbleOvalLeft size={24} />
-      <LuUser size={24} />
-    </div>
+    <nav>
+      <ul className="list-none p-0 m-0 flex gap-4">
+        {menuItems.map((item) => (
+          <li
+            key={item.id}
+            onClick={() => handleItemClick(item.id)}
+            className={`cursor-pointer p-2 rounded ${
+              activeItem === item.id ? "text-red-300" : ""
+            }`}
+          >
+            {item.icon}
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 };
 
